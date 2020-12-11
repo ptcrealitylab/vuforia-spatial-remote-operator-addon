c7b2219f4225ce966865e146f48df40c1bae5d9e
Ben Reynolds <breynolds@ptc.com>
2020-12-11 14:49:29 -0500

    adds license info

--- interfaces/desktopEditor/index.js
+++ interfaces/desktopEditor/index.js
@@ -1,6 +1,10 @@
-/**
- * Created by Ben Reynolds on 10/01/18.
- */
+/*
+* Copyright © 2018 PTC
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
 
 /**
  * Set to true to enable the hardware interface
