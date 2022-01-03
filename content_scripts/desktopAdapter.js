19daccd3d617866806cfe2db74847ca3621da77a
benptc <breynolds@ptc.com>
2022-01-03 13:46:17 -0500

    slows down vertical movement as you approach very top or bottom perspectives, to avoid camera singularities / funky movement as horizontal perimeter of orbit shinks

--- content_scripts/desktopAdapter.js
+++ content_scripts/desktopAdapter.js
@@ -196,7 +196,6 @@ const DEBUG_DISABLE_DROPDOWNS = false;
                         startX: touchPosition.x,
                         startY: touchPosition.y
                     }
-                    console.log('set synthetic pinch info: ', realityEditor.device.editingState.syntheticPinchInfo);
                 }
             }
         });
