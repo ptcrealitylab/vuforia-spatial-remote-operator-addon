5ccc3e2c511a7eaadcee03f3d44e22e6331e3cb9
benptc <breynolds@ptc.com>
2022-01-24 17:53:23 -0500

    template for VideoPlayback content_script

---
+++ content_scripts/VideoPlayback.js
@@ -0,0 +1,14 @@
+/*
+* Copyright © 2022 PTC
+*/
+
+createNameSpace('realityEditor.device');
+
+(function(exports) {
+    class VideoPlayback {
+        constructor() {
+        }
+    }
+
+    exports.VideoPlayback = VideoPlayback;
+})(realityEditor.device);
