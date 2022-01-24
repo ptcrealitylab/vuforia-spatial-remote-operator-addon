6024972d9e9756f72e7fe36038fcba41d00062b3
benptc <breynolds@ptc.com>
2022-01-24 13:40:31 -0500

    refactors video recording code into VideoServer class, creates a 10 second loop to write streamed images to small video files

--- interfaces/remoteOperatorUI/server.js
+++ interfaces/remoteOperatorUI/server.js
@@ -2,38 +2,38 @@ const cors = require('cors');
 const express = require('express');
 const expressWs = require('express-ws');
 const makeStreamRouter = require('./makeStreamRouter.js');
-const fs = require('fs');
+const VideoServer = require('./VideoServer.js');
+// const fs = require('fs');
 const path = require('path');
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-const ffmpeg = require('fluent-ffmpeg');
-const cp = require('child_process');
+// const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+// const ffmpeg = require('fluent-ffmpeg');
+// const cp = require('child_process');
 
 module.exports.start = function start() {
     const app = express();
-    ffmpeg.setFfmpegPath(ffmpegPath);
+    const videoServer = new VideoServer(path.join(__dirname, 'videos'));
 
-    let childProcesses = {};
-    let childProcessStatus = {};
-
+    // ffmpeg.setFfmpegPath(ffmpegPath);
+    // let childProcesses = {};
+    // let childProcessStatus = {};
     // from images (I think PNGs don't work, we could try converting to JPGs?)
-
-    if (!fs.existsSync(path.join(__dirname, 'images'))) {
-        fs.mkdirSync(path.join(__dirname, 'images'));
-    }
-
-    if (fs.existsSync(path.join(__dirname, 'images', 'color'))) {
-        processImages(path.join(__dirname, 'images', 'color'));
-    } else {
-        fs.mkdirSync(path.join(__dirname, 'images', 'color'));
-        processImages(path.join(__dirname, 'images', 'color'));
-    }
-
-    if (fs.existsSync(path.join(__dirname, 'images', 'depth'))) {
-        processImages(path.join(__dirname, 'images', 'depth'));
-    } else {
-        fs.mkdirSync(path.join(__dirname, 'images', 'depth'));
-        processImages(path.join(__dirname, 'images', 'depth'));
-    }
+    // if (!fs.existsSync(path.join(__dirname, 'images'))) {
+    //     fs.mkdirSync(path.join(__dirname, 'images'));
+    // }
+    //
+    // if (fs.existsSync(path.join(__dirname, 'images', 'color'))) {
+    //     processImages(path.join(__dirname, 'images', 'color'));
+    // } else {
+    //     fs.mkdirSync(path.join(__dirname, 'images', 'color'));
+    //     processImages(path.join(__dirname, 'images', 'color'));
+    // }
+    //
+    // if (fs.existsSync(path.join(__dirname, 'images', 'depth'))) {
+    //     processImages(path.join(__dirname, 'images', 'depth'));
+    // } else {
+    //     fs.mkdirSync(path.join(__dirname, 'images', 'depth'));
+    //     processImages(path.join(__dirname, 'images', 'depth'));
+    // }
 
     // if (fs.existsSync(path.join(__dirname, 'images', 'matrix'))) {
     //     // processImages(path.join(__dirname, 'images', 'matrix'));
@@ -42,264 +42,69 @@ module.exports.start = function start() {
     //     fs.mkdirSync(path.join(__dirname, 'images', 'matrix'));
     // }
 
-    function processImages(dirPath) {
-        console.log('process: ' + dirPath);
-        let files = fs.readdirSync(dirPath).filter(function(filename) {
-            return filename.includes('.png');
-        });
-        // console.log(files);
-
-        let imageType = dirPath.includes('color') ? 'color' : 'depth';
-
-        let inputWidth = 1920;
-        let inputHeight = 1080;
-        let width = inputWidth / 4;
-        let height = inputHeight / 4;
-        let outputPath = path.join(dirPath, imageType + '_' + width + 'x' + height + '.mp4');
-        console.log('OUTPUTTING VIDEO TO: ' + outputPath);
-
-        let inputPath = path.join(dirPath, imageType + '_%08d.png');
-        console.log('INPUT: ' + inputPath);
-
-        let args = [
-            '-r', '10',
-            '-f', 'image2pipe'
-        ];
-        if (imageType === 'color') {
-            args.push('-vcodec', 'mjpeg',);
-        }
-        args.push(
-            '-s', inputWidth + 'x' + inputHeight,
-            // '-i', inputPath,
-            '-i', '-',
-            '-vcodec', 'libx264',
-            '-crf', '25',
-            '-pix_fmt', 'yuv420p',
-            '-vf', 'scale=' + width + ':' + height + ',setsar=1:1',
-            outputPath
-        );
-
-        console.log(args);
-
-        childProcesses[imageType] = cp.spawn('ffmpeg', args);
-        childProcessStatus[imageType] = 'STARTED';
-
-        childProcesses[imageType].stdout.on('data', function(data) {
-            console.log('stdout data', data);
-        });
-
-        childProcesses[imageType].stderr.setEncoding('utf8');
-        childProcesses[imageType].stderr.on('data', function(data) {
-            console.log('stderr data', data);
-        });
-
-        childProcesses[imageType].on('close', function() {
-            console.log('finished');
-        });
-
-        console.log('created childProcess for ' + imageType);
-
-        // let proc = cp.spawn('ffmpeg', [
-        //     '-r', '10',
-        //     '-f', 'image2',
-        //     // '-vcodec', 'mjpeg',
-        //     '-s', inputWidth + 'x' + inputHeight,
-        //     '-i', inputPath,
-        //     '-vcodec', 'libx264',
-        //     '-crf', '25',
-        //     '-pix_fmt', 'yuv420p',
-        //     '-vf', 'scale=' + width + ':' + height + ',setsar=1:1',
-        //     outputPath
-        // ]);
-
-        // proc[imageType] = cp.spawn('ffmpeg', [
-        //     '-r', '10',
-        //     '-f', 'image2pipe',
-        //     '-vcodec', 'mjpeg',
-        //     '-s', '1920x1080',
-        //     '-i', '-',
-        //     '-vcodec', 'libx264',
-        //     '-crf', '25',
-        //     '-pix_fmt', 'yuv420p',
-        //     outputPath
-        // ]);
-
-        // proc.stdin.write(Buffer.from([
-        //     255, 0, 0,
-        //     0, 255, 0,
-        //     0, 255, 255,
-        //     255, 0, 255
-        // ]));
-        //
-        // proc.stdin.end();
-        //
-        // proc.stderr.pipe(process.stdout);
-
-        // const command = ffmpeg();
-        // // Use FFMpeg to create a video.
-        // // 8 consecutive frames, held for 5 seconds each, 30fps output, no audio
-        // // color_1642712727894.png
-        // let filename = dirPath.includes('/color') ? 'color' : 'depth';
-        // let inputFormat = path.join(dirPath, filename + '_%08d.png');
-        // console.log(inputFormat);
-        //
-        // let outputName = path.join(dirPath, filename + '-1920x1080.mp4');
-        //
-        // let timemark = null;
-        //
-        // command
-        //     .on('end', onEnd)
-        //     .on('progress', onProgress)
-        //     // .on('error', onError)
-        //     .input(inputFormat)
-        //     .fromFormat('image2pipe')
-        //     // .addInputOption('-vcodec', 'png')
-        //     // .inputFormat('png')
-        //     .inputFPS(30)
-        //     .output(outputName)
-        //     .outputFPS(30)
-        //     .noAudio()
-        //     .run();
-        //
-        // console.log('processing... ' + filename);
-        //
-        //
-        // function onEnd() {
-        //     console.log('Finished processing');
-        // }
-        //
-        // function onProgress(progress) {
-        //     if (progress.timemark !== timemark) {
-        //         timemark = progress.timemark;
-        //         console.log('Time mark: ' + timemark + '...');
-        //     }
-        // }
-
-        // function onError(err, _stdout, _stderr) {
-        //     console.log('Cannot process video: ' + err.message);
-        // }
-    }
-
-    // ffmpeg.setFfmpegPath(ffmpegPath);
-    // const command = ffmpeg();
-    // // Use FFMpeg to create a video.
-    // // 8 consecutive frames, held for 5 seconds each, 30fps output, no audio
-    // command
-    //     .input('assets/demo1/Sinewave3-1920x1080_%03d.png')
-    //     .inputFPS(1/5)
-    //     .output('assets/demo1/Sinewave3-1920x1080.mp4')
-    //     .outputFPS(30)
-    //     .noAudio()
-    //     .run();
-
-    // https://stackoverflow.com/questions/46073876/node-js-buffer-data-to-ffmpeg
-    // let outputPath = path.join(__dirname, '1.png');
+    // function processImages(dirPath) {
+    //     console.log('process: ' + dirPath);
+    //     let files = fs.readdirSync(dirPath).filter(function(filename) {
+    //         return filename.includes('.png');
+    //     });
+    //     // console.log(files);
     //
-    // let proc = cp.spawn('ffmpeg', [
-    //     '-hide_banner',
-    //     '-f', 'rawvideo',
-    //     '-pix_fmt', 'rgb24',
-    //     '-s', '2x2',
-    //     '-i', '-',
-    //     outputPath
-    // ]);
+    //     let imageType = dirPath.includes('color') ? 'color' : 'depth';
     //
-    // proc.stdin.write(Buffer.from([
-    //     255, 0, 0,
-    //     0, 255, 0,
-    //     0, 255, 255,
-    //     255, 0, 255
-    // ]));
+    //     let inputWidth = 1920;
+    //     let inputHeight = 1080;
+    //     let width = inputWidth / 4;
+    //     let height = inputHeight / 4;
+    //     let outputPath = path.join(dirPath, imageType + '_' + width + 'x' + height + '.mp4');
+    //     console.log('OUTPUTTING VIDEO TO: ' + outputPath);
     //
-    // proc.stdin.end();
+    //     let inputPath = path.join(dirPath, imageType + '_%08d.png');
+    //     console.log('INPUT: ' + inputPath);
     //
-    // proc.stderr.pipe(process.stdout);
+    //     let args = [
+    //         '-r', '10',
+    //         '-f', 'image2pipe'
+    //     ];
+    //     if (imageType === 'color') {
+    //         args.push('-vcodec', 'mjpeg',);
+    //     }
+    //     args.push(
+    //         '-s', inputWidth + 'x' + inputHeight,
+    //         // '-i', inputPath,
+    //         '-i', '-',
+    //         '-vcodec', 'libx264',
+    //         '-crf', '25',
+    //         '-pix_fmt', 'yuv420p',
+    //         '-vf', 'scale=' + width + ':' + height + ',setsar=1:1',
+    //         outputPath
+    //     );
+    //
+    //     console.log(args);
+    //
+    //     childProcesses[imageType] = cp.spawn('ffmpeg', args);
+    //     childProcessStatus[imageType] = 'STARTED';
+    //
+    //     childProcesses[imageType].stdout.on('data', function(data) {
+    //         console.log('stdout data', data);
+    //     });
+    //
+    //     childProcesses[imageType].stderr.setEncoding('utf8');
+    //     childProcesses[imageType].stderr.on('data', function(data) {
+    //         console.log('stderr data', data);
+    //     });
+    //
+    //     childProcesses[imageType].on('close', function() {
+    //         console.log('finished');
+    //     });
+    //
+    //     console.log('created childProcess for ' + imageType);
+    // }
 
-    // let outputName = path.join(__dirname, 'video-1920x1080.mp4');
-    // let command = ffmpeg();
-    // command
-    //     // .on('end', onEnd)
-    //     // .on('progress', onProgress)
-    //     // .on('error', onError)
-    //     // .input(inputFormat)
-    //     .inputOptions('-i -')
-    //     .fromFormat('image2pipe')
-    //     // .addInputOption('-vcodec', 'png')
-    //     // .inputFormat('png')
-    //     .inputFPS(30)
-    //     .output(outputName)
-    //     .outputFPS(30)
-    //     .noAudio()
-    //     .run();
-
-    let counter = 0;
     app.use(cors());
     expressWs(app);
     const streamRouter = makeStreamRouter(app);
     streamRouter.onFrame(function(rgb, depth, pose) {
-        // console.log(rgb, depth, pose);
-        const zeroPad = (num, places) => String(num).padStart(places, '0');
-
-        let colorFilename = 'color_' + zeroPad(counter, 8) + '.png'; // + Math.floor(Math.random() * 1000)
-        let depthFilename = 'depth_' + zeroPad(counter, 8) + '.png';
-        let matrixFilename = 'matrix_' + zeroPad(counter, 8) + '.png';
-        counter++;
-
-        if (typeof childProcesses['color'] !== 'undefined' && childProcessStatus['color'] === 'STARTED') {
-            console.log('write frame to color stdin');
-            childProcesses['color'].stdin.write(rgb);
-        }
-
-        // if (typeof childProcesses['depth'] !== 'undefined' && childProcessStatus['depth'] === 'STARTED') {
-        //     console.log('write frame to depth stdin');
-        //     childProcesses['depth'].stdin.write(depth);
-        // }
-
-        if (counter > 30) {
-            if (typeof childProcesses['color'] !== 'undefined' && childProcessStatus['color'] === 'STARTED') {
-                console.log('end color process');
-                childProcesses['color'].stdin.setEncoding('utf8');
-                childProcesses['color'].stdin.write('q');
-                // childProcesses['color'].exit();
-
-                childProcesses['color'].stdin.end();
-
-                childProcessStatus['color'] = 'ENDING';
-
-                // childProcesses['color'].stderr.pipe(childProcesses['color'].stdout);
-                // delete childProcesses['color'];
-            }
-
-            // if (typeof childProcesses['depth'] !== 'undefined' && childProcessStatus['depth'] === 'STARTED') {
-            //     console.log('end depth process');
-            //     // childProcesses['depth'].stdin.end();
-            //     childProcesses['depth'].stdin.setEncoding('utf8');
-            //     childProcesses['depth'].stdin.write('q');
-            //     // childProcesses['depth'].exit();
-            //     childProcessStatus['depth'] = 'ENDING';
-            //
-            //     // childProcesses['depth'].stderr.pipe(childProcesses['depth'].stdout);
-            //     // delete childProcesses['depth'];
-            // }
-
-            counter = 0;
-        }
-
-        // let colorFilename = 'color_' + Date.now() + '.png'; // + Math.floor(Math.random() * 1000)
-        // let depthFilename = 'depth_' + Date.now() + '.png';
-        // let matrixFilename = 'matrix_' + Date.now() + '.png';
-
-        fs.writeFile(path.join(__dirname, 'images', 'color', colorFilename), rgb, function() {
-            // console.log('wrote color image');
-        });
-
-        fs.writeFile(path.join(__dirname, 'images', 'depth', depthFilename), depth, function() {
-            // console.log('wrote depth image');
-        });
-
-        fs.writeFile(path.join(__dirname, 'images', 'matrix', matrixFilename), pose, function() {
-            // console.log('wrote matrix image');
-        });
+        videoServer.onFrame(rgb, depth, pose);
     });
 
     let allWebsockets = [];
