459af310c28fe5f5bbee47d7a1e8845f361a1902
Anna Fuste <afuste@ptc.com>
2022-02-02 13:27:00 -0500

    Hololens rendering in realtime. Human avatar (static)

--- content_scripts/SocketDataSource.js
+++ content_scripts/SocketDataSource.js
@@ -5,22 +5,37 @@ createNameSpace('realityEditor.gui.ar.desktopRenderer');
         constructor() {
             this.lastDataTime = -1;
             this.poses = [];
-
+            
             const url = 'ws://' + window.location.hostname + ':31337/';
             this.socket = new WebSocket(url);
 
             this.onMessage = this.onMessage.bind(this);
 
             this.socket.addEventListener('message', this.onMessage);
+            
+            this.hololensPosition = {x: 0, y: 0, z: 0};
+            this.hololensRotation = {x: 0, y: 0, z: 0};
+            this.hololensRightHandPosition = {x: 0, y: 0, z: 0};
+            this.hololensLeftHandPosition = {x: 0, y: 0, z: 0};
         }
 
         onMessage(event) {
             
             try {
                 
-                console.log('Message received', event.data);
-
                 let msg = JSON.parse(event.data);
+                
+                if (msg.command === '/update/hololensPose') {
+
+                    //console.log('Got Hololens Pose: ', msg.position);
+                    
+                    this.hololensPosition = msg.position;
+                    this.hololensRotation = msg.rotation;
+                    this.hololensRightHandPosition = msg.handpositionright;
+                    this.hololensLeftHandPosition = msg.handpositionleft;
+                    
+                    
+                }
 
                 if (msg.command !== '/update/humanPoses') {
                     return;
