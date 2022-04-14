3f06b99dfd55ac8b27217125a766b86c6740eb50
benptc <breynolds@ptc.com>
2022-04-14 17:25:45 -0400

    fairly robust initial implementation of menu bar, with a handful of integrated items with keyboard shortcuts

--- content_scripts/setupMenuBar.js
+++ content_scripts/setupMenuBar.js
@@ -2,6 +2,9 @@ createNameSpace('realityEditor.gui');
 
 (function(exports) {
     let menuBar = null;
+
+    // sets up the initial contents of the menuBar
+    // other modules can add more to it by calling getMenuBar().addItemToMenu(menuName, menuItem)
     exports.setupMenuBar = () => {
         const MenuBar = realityEditor.gui.MenuBar;
         const Menu = realityEditor.gui.Menu;
@@ -11,22 +14,31 @@ createNameSpace('realityEditor.gui');
         // menuBar.addMenu(new Menu('File'));
         // menuBar.addMenu(new Menu('Edit'));
         menuBar.addMenu(new Menu('View'));
+        menuBar.addMenu(new Menu('History'));
         menuBar.addMenu(new Menu('Help'));
 
-        const togglePointClouds = new MenuItem('Point Clouds', { shortcutKey: 'M', toggle: true, defaultVal: true, disabled: true }, () => {
-            console.log('toggle point clouds');
+        const togglePointClouds = new MenuItem('Point Clouds', { shortcutKey: 'M', toggle: true, defaultVal: true, disabled: true }, (value) => {
+            console.log('toggle point clouds', value);
         });
         menuBar.addItemToMenu('View', togglePointClouds);
 
-        const toggleSpaghetti = new MenuItem('Spaghetti Map', { shortcutKey: 'N', toggle: true, defaultVal: false, disabled: true }, () => {
-            console.log('toggle spaghetti map');
+        const toggleSpaghetti = new MenuItem('Spaghetti Map', { shortcutKey: 'N', toggle: true, defaultVal: false, disabled: true }, (value) => {
+            console.log('toggle spaghetti map', value);
         });
         menuBar.addItemToMenu('View', toggleSpaghetti);
 
-        const toggleModelTexture = new MenuItem('Model Texture', { shortcutKey: 'T', toggle: true, defaultVal: true }, () => {
-            console.log('toggle model texture');
-        });
+        const toggleModelTexture = new MenuItem('Model Texture',
+            { shortcutKey: 'T', toggle: true, defaultVal: true }, null); // other module can attach a callback later
         menuBar.addItemToMenu('View', toggleModelTexture);
+
+        const resetCamera = new MenuItem('Reset Camera Position', { shortcutKey: 'ESCAPE' }, null);
+        menuBar.addItemToMenu('View', resetCamera);
+
+        const resetRzvHistory = new MenuItem('Reset Paths', { shortcutKey: 'R' }, null);
+        menuBar.addItemToMenu('History', resetRzvHistory);
+
+        const toggleRzvHistory = new MenuItem('Toggle Paths', { shortcutKey: 'E', toggle: true, defaultVal: true }, null);
+        menuBar.addItemToMenu('History', toggleRzvHistory);
 
         const gettingStarted = new MenuItem('Getting Started', null, () => {
             console.log('open getting started in new tab');
